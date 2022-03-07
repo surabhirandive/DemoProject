6ca1e849dcba4f86db9a97197f87e14552ea37be
surabhirandive <Surabhi Randive@LAPTOP-GMPSTSU7>
2022-03-07 14:11:21 +0530

    committing to branchx

--- src/co/in/test/Test.java
+++ src/co/in/test/Test.java
@@ -4,9 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 
-		int x = 10;
-		int y = 20;
-		System.out.println(x + y);
+		int data1 = 10;
+		int data2 = 20;
+		System.out.println(data1 + data2);
 
 	}
 
