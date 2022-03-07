816ad98b8b653cc08b4bb32c7d73279d03eb2e80
surabhirandive <Surabhi Randive@LAPTOP-GMPSTSU7>
2022-03-07 14:03:01 +0530

    Commit Test class...

---
+++ src/co/in/test/Test.java
@@ -0,0 +1,13 @@
+package co.in.test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		int x = 10;
+		int y = 20;
+		System.out.println(x + y);
+
+	}
+
+}
